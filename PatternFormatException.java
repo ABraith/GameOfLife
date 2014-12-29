741949390b17e88bd93f5a037d0ee7ef7bd26959
ABraith <ab2153@cam.ac.uk>
2014-12-29 15:37:17 +0000

    Upload

---
+++ PatternFormatException.java
@@ -0,0 +1,18 @@
+
+
+package uk.ac.cam.ab2153.tick5;
+
+@SuppressWarnings("serial")
+public class PatternFormatException
+    extends Exception
+{
+  PatternFormatException()
+  {
+    super();
+  }
+
+  PatternFormatException(String msg)
+  {
+    super(msg);
+  }
+}
